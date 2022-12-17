46693534ed7563cec081ab0004cf475109e68ed0
justMadridist <justMadridist@gmail.com>
2022-12-17 11:55:49 +0300

    test configs (not to rewrite in new devices)

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import time
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("Should be like: pytest -s -v --browser_name=(chrome or firefox) test_cmd.py")
+    yield browser
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
